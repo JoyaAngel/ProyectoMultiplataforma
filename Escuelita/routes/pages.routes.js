f4e595c7e33aa85ee508b9aefb0fc3e693503282
GirTabAleph <nineteencycles@gmail.com>
2026-04-29 21:57:16 -0600

    Actualización del cotrol de páginas para usar el render de horarios y grupos

--- Escuelita/routes/pages.routes.js
+++ Escuelita/routes/pages.routes.js
@@ -5,10 +5,14 @@ const {
     renderDashboard,
     renderAlumnos,
     renderProfesores,
-    renderEntidades
+    renderEntidades,
+    renderGrupos,
+    renderHorarios
 } = require('../controllers/pages.controller');
 
+//principal
 router.get('/', renderDashboard);
+
 router.get('/dashboard', renderDashboard);
 
 router.get('/alumnos', renderAlumnos);
@@ -16,5 +20,9 @@ router.get('/alumnos', renderAlumnos);
 router.get('/profesores', renderProfesores);
 
 router.get('/entidades', renderEntidades);
+
+router.get('/grupos', renderGrupos);
+
+router.get('/horarios', renderHorarios);
 
 module.exports = router;
