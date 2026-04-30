3dd0f98890bb3a71cfe5b25498122aa0218d6562
GirTabAleph <nineteencycles@gmail.com>
2026-04-29 21:55:18 -0600

    Controlador para grupos

---
+++ Escuelita/controllers/grupos.controller.js
@@ -0,0 +1,53 @@
+const { Grupo } = require("../models");
+const router = require("../routes/pages.routes");
+
+async function getGrupos(req, res) {
+
+    try{
+
+        const grupos = await Grupo.findAll();
+        return res.json({data: grupos})
+
+    }catch(e){
+
+        console.error('Error al obtener los grupos:', e);
+        return res.status(500).json({ error: 'Error al obtener los grupos' });
+
+    }
+
+}
+
+async function createGrupo(req, res) {
+
+    try{
+
+        const { nombre_grupo } = req.body;
+
+        if(!nombre_grupo){
+
+            return res.status(400).json({
+                error: 'Faltan parámetros obligados: nombre_grupo'
+            });
+
+        }
+
+        const nuevoGrupo = await Grupo.create({nombre_grupo});
+
+        return res.status(201).json({
+            message: "Horario creado correctamente",
+            data: nuevoGrupo
+        });
+
+    }catch(e){
+
+        console.error("Error al crear el grupo:", e);
+        return res.status(500).json({ error: "Error al crear el grupo" });
+
+    }
+
+}
+
+module.exports = {
+    getGrupos,
+    createGrupo
+}
