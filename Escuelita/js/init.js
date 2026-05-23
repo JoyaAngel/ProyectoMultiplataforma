0411fef0128ff3ab839228f802bab722a23f1891
JoyaAngel <angelgabriel.joya@comunidad.unam.mx>
2026-05-22 20:04:40 -0600

    Implementación de PUT

---
+++ Escuelita/js/init.js
@@ -0,0 +1,5 @@
+import { openForm as openFormFunc, closeForm as closeFormFunc } from "/assets/js/features/formLoader.js";
+
+// Exponer funciones globalmente
+window.openForm = openFormFunc;
+window.closeForm = closeFormFunc;
