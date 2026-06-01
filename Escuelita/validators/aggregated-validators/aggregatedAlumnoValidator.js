cb86d458fe2ce30629d440548c03d5a618a8a11e
GirTabAleph <nineteencycles@gmail.com>
2026-05-31 23:10:40 -0600

    Validadores que hacen agregación de los valdiadores individuales

---
+++ Escuelita/validators/aggregated-validators/aggregatedAlumnoValidator.js
@@ -0,0 +1,51 @@
+const { isValidNumeroCuenta } = require('../individual-validators/numeroCuentaValidator');
+const { isCurpFormatValid } = require('../individual-validators/curpFormatValidator');
+const { matchesGivenLength } = require('../individual-validators/anyLengthValidator');
+const { isValidSexo } = require('../individual-validators/sexoValidator');
+const { isValidEmail } = require('../individual-validators/emailValidator');
+const { isIsoDateCompliant } = require('../individual-validators/isoCompliantDateValidator');
+const { Alumno} = require("../../models");
+
+function performAlumnoValidations(alumno){
+
+    const {
+        numero_cuenta,
+        curp,
+        telefono,
+        sexo,
+        correo_electronico,
+        fecha_nacimiento
+    } = alumno;
+
+    if(!isValidNumeroCuenta(numero_cuenta)){
+        return 'El número de cuenta no coincide a nueve dígitos.'
+    }
+
+    if(!isCurpFormatValid(curp)){
+        return 'La CURP no cumple el formato obligatorio.'
+    }
+
+    if(!matchesGivenLength(10, telefono)){
+        return 'El teléfono debe ser a 10 dígitos seguidos y sin espacios.'
+    }
+
+    if(!isValidSexo(sexo)){
+        return 'El sexo no debe de ser de más de un caracter, y debe se ser M o F'
+    }
+
+    if(!isValidEmail(correo_electronico)){
+        return 'El correo electrónico no cumple la estructura "usuario@dominio.extensión" '
+    }
+
+    console.log(`Fecha recibida: ${fecha_nacimiento}`);
+    if(!isIsoDateCompliant(fecha_nacimiento)){
+        return 'Formato de fecha no reconocido. Debe de ser AAAA-MM-DD.'
+    }
+
+    return null;
+
+}
+
+module.exports = {
+    performAlumnoValidations
+}
